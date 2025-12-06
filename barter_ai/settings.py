14f4cc77a9da2b3a4aae83c4e296496395c31137
Shahanashalu <shalushahna1@gmail.com>
2025-12-06 16:51:09 +0530

    Fix staticfiles Pathlib issue

--- barter_ai/settings.py
+++ barter_ai/settings.py
@@ -129,8 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]   # for local dev
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')      # for render deploy
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',    # correct for Pathlib
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 MEDIA_URL = '/media/'
