8a06548d9a0baeb4158f5831e9a474f2852ef06b
Shahanashalu <shalushahna1@gmail.com>
2025-12-10 13:05:51 +0530

    Create new superuser

--- barter_ai/urls.py
+++ barter_ai/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('products/all/', product_list, name='all_products'),
     path('barter/', include('barter.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
